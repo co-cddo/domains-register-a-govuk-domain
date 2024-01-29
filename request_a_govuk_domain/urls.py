53acbbed835a03bcb03d0e2d21df89bc38b5cd8c
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-01-29 14:39:04 +0000

    Add view templates and style

--- request_a_govuk_domain/urls.py
+++ request_a_govuk_domain/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .request import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('request', views.index, name="index"),
 ]
