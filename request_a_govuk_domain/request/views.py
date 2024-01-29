277a793cafa57e52482f644e12e6c2c127eadd13
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-01-29 15:20:06 +0000

    Add the govuk design system

--- request_a_govuk_domain/request/views.py
+++ request_a_govuk_domain/request/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from .forms import NameForm
 
 
 def index(request):
     template = loader.get_template("index.html")
+    form = NameForm()
     context = {
-        "title": "ok"
+        "title": "ok",
+        "form": form
     }
+
     return HttpResponse(template.render(context, request))
