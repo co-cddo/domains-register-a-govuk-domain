f076004fda116e9871d0f18de97f2bf1d985739e
Dharshana Vanderbona <dharshana.vanderbona@digital.cabinet-office.gov.uk>
2024-01-30 11:37:42 +0000

    New Base form class to provide styling the labels as it is not provided by default in Django.
    - Any form requiring label styling should extend from FormWithLabelStyle.
    - Default label style is 'label_style' provided as an argument to the init of FormWithLabelStyle

---
+++ request_a_govuk_domain/request/base_from.py
@@ -0,0 +1,61 @@
+from django import forms
+from django.forms import BoundField
+
+
+class BoundFieldWithLabelClass(BoundField):
+    """
+    Custom bound field that provides the label styling
+    """
+
+    def __init__(self, label_style: str, *args):
+        self.label_style = label_style
+        super().__init__(*args)
+
+    def label_tag(self, contents=None, attrs=None, label_suffix=None, tag=None):
+        """
+        Override the label generation so we can add the style
+        :param contents:
+        :param attrs:
+        :param label_suffix:
+        :param tag:
+        :return:
+        """
+        if not attrs:
+            attrs = {}
+        if self.label_style:
+            attrs["class"] = self.label_style
+        return super().label_tag(contents, attrs, label_suffix, tag)
+
+
+class FieldProxy(object):
+    """
+    Proxy that wraps any Field object and provide
+    an instance of BoundFieldWithLabelClass instance on the
+    return value of get_bound_field
+    """
+
+    def __init__(self, proxied, label_style):
+        self.proxied = proxied
+        self.label_style = label_style
+
+    def __getattr__(self, item):
+        return getattr(self.proxied, item)
+
+    def get_bound_field(self, form, field_name):
+        # Intercept the bound field and return the proxy that supports label styles
+        return BoundFieldWithLabelClass(self.label_style, form, self.proxied, field_name, )
+
+
+class FormWithLabelStyle(forms.Form):
+    """
+    Custom class that updated the fields so that we can apply style
+    attributes to the labels as it is not supported by default.
+    """
+
+    def __init__(self, label_style: str = "govuk-label", *args):
+        super().__init__(*args)
+        proxied_fields = dict()
+        for field_name in self.fields:
+            proxied_fields[field_name] = FieldProxy(self.fields[field_name], label_style)
+        # Replace the fields with the proxies, that provide the label styling.
+        self.fields = proxied_fields
--- request_a_govuk_domain/request/forms.py
+++ request_a_govuk_domain/request/forms.py
@@ -1,12 +1,25 @@
 from django import forms
+from django.forms import CharField, DateField, BooleanField, DateTimeInput
+
+from .base_from import FormWithLabelStyle
 
 
-class NameForm(forms.Form):
-
-    your_name = forms.CharField(
+class NameForm(FormWithLabelStyle):
+    your_name = CharField(
         label="Your name",
         max_length=100,
         widget=forms.TextInput(
             attrs={"class": "govuk-input"}
         )
     )
+    date = DateField(
+        label="Your Birthday",
+        widget=DateTimeInput(
+            attrs={"class": "govuk-datetime"}
+        )
+    )
+
+    yes_no = BooleanField(label="Are you good",
+                          widget=forms.CheckboxInput(
+                              attrs={"class": "govuk-checkbox"}
+                          ))
