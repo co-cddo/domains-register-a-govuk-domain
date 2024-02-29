274c90c3199b5f5c8f6804ec8e4c3c65b6d83e04
Sravan Jinna <sravan.jinna@digital.cabinet-office.gov.uk>
2024-02-29 10:07:24 +0000

    With upload tests and config for using the Cypress Studio.
    Minor refactor in files names of tests.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    experimentalStudio: true,
+    baseUrl: 'http://0.0.0.0:8000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
