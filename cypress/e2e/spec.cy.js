dd763060116eb80f2ed38d82b0ffe044f6bd4a13
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-02-29 15:39:17 +0000

    Improve the test by checking for errors

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,26 +1,29 @@
 describe('name spec', () => {
   it('passes', () => {
     cy.visit('http://0.0.0.0:8000/name/')
-    
+
     //Get an input, type into it
     cy.get('.govuk-input').type('GOV UK')
 
     //Click Save
     cy.get('.govuk-button').click()
 
-    //Get an input, type into it
-    cy.get('.govuk-input').type('something@some.gov.uk')
+    // There should be an error
+    cy.get('h2').should('exist')
+    cy.get('h2').should('have.value', 'There is a problem')
 
-    //Click Save
+    //Retrying with a correct email
+    cy.get('.govuk-input').type('something@some.gov.uk')
     cy.get('.govuk-button').click()
 
-    //Confirm Screen
-    cy.get('.govuk-heading-xl').should('have.value', '')
+    // No error message this time
+    cy.get('h2').should('not.exist')
+
   })
 
   // it('fails', () => {
   //   cy.visit('http://0.0.0.0:8000/name/')
-    
+
   //   //Get an input, type into it
   //   cy.get('.govuk-input').should('not.to.match', ':empty')
 
