201639913d98c3d791ffe60ebb702f8d8e71ddcd
Sravan Jinna <sravan.jinna@digital.cabinet-office.gov.uk>
2024-03-04 11:46:45 +0000

    Not needed in this PR.
    Already changed in main with Max's work.

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    experimentalStudio: true,
-    baseUrl: 'http://0.0.0.0:8000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
