4cf173024f2db19aa55d39c96b9497363e8fa7e1
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-03-06 16:06:00 +0000

    Add test to check the Exemption path
    Only start for now. To be completed later

---
+++ cypress/e2e/exemption-happy-path.cy.js
@@ -0,0 +1,35 @@
+describe('Happy passes', () => {
+  it('performs a full transaction', () => {
+    cy.visit('http://0.0.0.0:8000/')
+
+    cy.get('h1').should('include.text', 'Which .gov.uk Approved Registrar organisation are you from?')
+    cy.get('select.govuk-select').should('exist')
+    cy.get('select.govuk-select').select('34SP.com')
+    cy.get('.govuk-button').click()
+
+
+    cy.get('h1').should('include.text', 'What is your email address?')
+    cy.get('.govuk-input').type('something@some.gov.uk')
+    cy.get('.govuk-button').click()
+
+
+    cy.get('h1').should('include.text', 'Which of the following best describes your registrant\'s organisation?')
+    cy.get('#id_registrant_type_1').click()
+    cy.get('.govuk-button').click()
+
+    cy.get('h1').should('include.text', 'What is your registrant’s organisation name?')
+    cy.get('.govuk-input').type('HMRC')
+    cy.get('.govuk-button').click()
+
+    cy.get('h1').should('include.text', 'Why do you want a .gov.uk domain name?')
+    cy.get('#id_domain_purpose_1').click()
+    cy.get('.govuk-button').click()
+
+    cy.get('h1').should('include.text', 'Does your registrant have an exemption from using the GOV.UK website?')
+    cy.get('#id_exe_radio_1').click()
+    cy.get('.govuk-button').click()
+
+    cy.get('h1').should('include.text', 'Upload evidence of the exemption')
+
+  })
+})
