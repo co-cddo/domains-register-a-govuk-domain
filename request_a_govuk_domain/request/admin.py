21185fd0332bec2281c63d8d4fbcfeccbc050173
Jim Narey <james.narey@digital.cabinet-office.gov.uk>
2024-03-08 19:16:33 +0000

    Add a basic admin interface
    This enables staff users to view applications and edit reviews.
    
    Superusers can edit applications as well as users and groups.
    
    This currently doesn't include views for Person and Organisation.
    At some stage, at least, we'll want staff users to be able to
    edit the list of registrars.

--- request_a_govuk_domain/request/admin.py
+++ request_a_govuk_domain/request/admin.py
@@ -1,3 +1,47 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import Application, CentralGovernmentAttributes, Review
+
+
+class DomainRegistrationUserAdmin(UserAdmin):
+    def has_module_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        return False
+
+
+class DomainRegistrationGroupAdmin(GroupAdmin):
+    def has_module_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        return False
+
+
+class CentralGovernmentAttributesInline(admin.StackedInline):
+    model = CentralGovernmentAttributes
+    can_delete = False
+    verbose_name_plural = "Central Government Attributes"
+
+
+class ReviewInline(admin.StackedInline):
+    model = Review
+    can_delete = False
+    verbose_name_plural = "Reviews"
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    model = Application
+    inlines = [CentralGovernmentAttributesInline, ReviewInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, DomainRegistrationUserAdmin)
+
+admin.site.unregister(Group)
+admin.site.register(Group, DomainRegistrationGroupAdmin)
+
+admin.site.register(Application, ApplicationAdmin)
