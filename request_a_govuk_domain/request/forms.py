7fb93fb8c2320952e6335d00477e202294cd07d0
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-03-13 13:58:34 +0000

    Populate the registrar dropdown from the db
    and not from a CSV

--- request_a_govuk_domain/request/forms.py
+++ request_a_govuk_domain/request/forms.py
@@ -16,7 +16,7 @@ from crispy_forms_gds.layout import (
 )
 
 from .utils import organisations_list
-from .models.organisation import RegistrantTypeChoices
+from .models.organisation import RegistrantTypeChoices, Registrar
 
 
 class NameForm(forms.Form):
@@ -464,9 +464,12 @@ class RegistrarForm(forms.Form):
     Registrar Form with organisations choice fields
     """
 
+    registrars = [("", "Select an item from the list")] + \
+        list((f"registrar-{registrar.id}", registrar.name) for registrar in Registrar.objects.all())
+
     organisations_choice = forms.ChoiceField(
         label="Choose your organisation",
-        choices=tuple(organisations_list()),
+        choices=registrars,
         widget=forms.Select(attrs={"class": "govuk-select"}),
         required=True,
     )
