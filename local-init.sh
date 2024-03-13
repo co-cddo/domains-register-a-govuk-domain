a0bdb7807b1f9311c5af1df41838c34b0fff2829
Jim Narey <james.narey@digital.cabinet-office.gov.uk>
2024-03-13 20:29:32 +0000

    Replace application seed data with mgt command
    This avoids hardcoding references to objects and is therefore less
    brittle.

--- local-init.sh
+++ local-init.sh
@@ -3,4 +3,5 @@
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py create_reviewer_group
-python manage.py loaddata ./seed/users.json ./seed/request.json
+python manage.py loaddata ./seed/users.json
+python manage.py create_sample_application
---
+++ request_a_govuk_domain/request/management/commands/create_sample_application.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from request_a_govuk_domain.request import models
+
+
+PERSON_NAMES = ["Bob Roberts", "Peter Peters", "Olivia Oliver"]
+
+REGISTRANT_ORG = "Ministry of Domains"
+REGISTRAR = "The Registrar Co."
+
+DOMAIN_NAME = "ministryofdomains.gov.uk"
+DOMAIN_PURPOSE = "Web site"
+
+
+class Command(BaseCommand):
+    help = "Create a sample application for local testing"
+
+    def handle(self, *args, **options):
+        persons = [models.Person.objects.create(name=name) for name in PERSON_NAMES]
+
+        registrar = models.Registrar(name=REGISTRAR)
+        registrant = models.Registrant(
+            name=REGISTRANT_ORG,
+            type=models.RegistrantTypeChoices.central_government,
+        )
+
+        application = models.Application(
+            domain_name=DOMAIN_NAME,
+            applicant=persons[0],
+            registrant_person=persons[1],
+            responsible_person=persons[2],
+            registrant_org=registrant,
+            registrar=registrar,
+            written_permission_evidence="",
+        )
+
+        registrar.save()
+        registrant.save()
+        application.save()
+
+        models.CentralGovernmentAttributes.objects.create(application=application)
+        models.Review.objects.create(application=application)
