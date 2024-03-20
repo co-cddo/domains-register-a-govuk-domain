b9dabf88e92b8f8658e19b2276beb4ed77a21739
Jim Narey <jimnarey@me.com>
2024-03-20 15:29:37 +0000

    Copy sample documents into media root

--- request_a_govuk_domain/request/management/commands/create_sample_data.py
+++ request_a_govuk_domain/request/management/commands/create_sample_data.py
@@ -1,6 +1,12 @@
+import os
+import shutil
 from django.core.management.base import BaseCommand
 from request_a_govuk_domain.request import models
 
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+SEED_DOCS_PATH = os.path.join(SCRIPT_PATH, "..", "..", "..", "..", "seed", "documents")
+MEDIA_ROOT_PATH = os.path.join(SCRIPT_PATH, "..", "..", "..", "media")
 
 PERSON_NAMES = ["Bob Roberts", "Peter Peters", "Olivia Oliver"]
 
@@ -10,11 +16,21 @@ REGISTRAR_NAMES = ["WeRegister", "Registrations R Us", "Fantastic Registrar", "H
 DOMAIN_NAME = "ministryofdomains.gov.uk"
 DOMAIN_PURPOSE = "Web site"
 
+WRITTEN_PERMISSION_FN = "written_permission.png"
+MINISTERIAL_REQUEST_FN = "ministerial_request.png"
+POLICY_TEAM_EXEMPTION_FN = "policy_team_exception.png"
+
 
 class Command(BaseCommand):
     help = "Create a sample application for local testing"
 
     def handle(self, *args, **options):
+        for file in os.listdir(SEED_DOCS_PATH):
+            try:
+                shutil.copy(os.path.join(SEED_DOCS_PATH, file), MEDIA_ROOT_PATH)
+            except shutil.SameFileError:
+                pass
+
         # delete existing data in case this is run multiple times
         models.RegistryPublishedPerson.objects.all().delete()
         models.RegistrarPerson.objects.all().delete()
@@ -56,10 +72,15 @@ class Command(BaseCommand):
             registry_published_person=registry_published_person,
             registrant_org=registrants[0],
             registrar_org=application_registrar,
-            written_permission_evidence="something",
+            written_permission_evidence=WRITTEN_PERMISSION_FN,
         )
 
         application.save()
 
-        models.CentralGovernmentAttributes.objects.create(application=application)
+        models.CentralGovernmentAttributes.objects.create(
+            application=application,
+            ministerial_request_evidence=MINISTERIAL_REQUEST_FN,
+            gds_exemption_evidence=POLICY_TEAM_EXEMPTION_FN,
+        )
+
         models.Review.objects.create(application=application)
