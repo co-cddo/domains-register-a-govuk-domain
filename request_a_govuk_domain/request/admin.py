f6adbbb8d5ac324c8c091897f5481ef07f44ff26
Dharshana Vanderbona <dharshana.vanderbona@digital.cabinet-office.gov.uk>
2024-03-21 12:46:27 +0000

    Experiment to get readonly link for written permission

--- request_a_govuk_domain/request/admin.py
+++ request_a_govuk_domain/request/admin.py
@@ -1,17 +1,31 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.admin import GroupAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.utils.html import format_html
 
 from .models import Application, CentralGovernmentAttributes, Review
 
+
 class ReviewerReadOnlyFieldsMixin:
+
     def get_readonly_fields(self, request, obj=None):
         if request.user.is_superuser:
             return []
         else:
-            return [field.name for field in self.model._meta.fields]
+            # TODO: We need to include written_permission only if self is application model
+            return [field.name for field in self.model._meta.fields if field.name != "written_permission_evidence"] + [
+                "written_permission"]
+
+    def get_fields(self, request, obj=None):
+        # TODO: We need to include written_permission only if self is application model
+        return [field.name for field in self.model._meta.fields if field.name != "written_permission_evidence"] + [
+            "written_permission"]
+
+    def written_permission(self, obj):
+        # link = reverse("admin:view_permission_evidence", args=[obj.written_permission_evidence.name])
+        return format_html('<a href="{}">{}</a>', "link", "View written permission evidence")
 
 
 class DomainRegistrationUserAdmin(UserAdmin):
@@ -44,7 +58,9 @@ class ApplicationAdmin(ReviewerReadOnlyFieldsMixin, admin.ModelAdmin):
     model = Application
     inlines = [CentralGovernmentAttributesInline, ReviewInline]
 
-
+    def get_form(self, request, obj=None, **kwargs):
+        _form = super().get_form(request, obj, **kwargs)
+        return _form
 
 
 admin.site.unregister(User)
