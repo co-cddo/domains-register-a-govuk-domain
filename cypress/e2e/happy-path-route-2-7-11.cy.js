9879df8c93e58d70aecda759e4f03d337b2f7fc2
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-03-27 15:20:20 +0000

    Route 2-7-11: exemption fail

---
+++ cypress/e2e/happy-path-route-2-7-11.cy.js
@@ -0,0 +1,19 @@
+import './base.cy'
+
+describe('Happy path - route 2 - 5', () => {
+  it('performs a full transaction', () => {
+    cy.goToRegistrarDetails()
+    cy.fillOutRegistrarDetails('WeRegister', 'Joe Bloggs', '01225672345', 'joe@example.org')
+
+    cy.checkPageTitleIncludes('Who is this domain name for?')
+    cy.chooseRegistrantType(1) // Central government -> Route 2
+
+    cy.checkPageTitleIncludes('Why do you want a .gov.uk domain name?')
+    cy.chooseDomainPurpose(1) // Email+web -> Route 7
+
+    cy.checkPageTitleIncludes('Does your registrant have an exemption from using the GOV.UK website?')
+    cy.selectYesOrNo('exemption', 'no')
+
+    cy.checkPageTitleIncludes('Your registrant cannot get approval for a .gov.uk domain name')
+  })
+})
