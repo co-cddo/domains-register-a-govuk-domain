2a66ae83465c9a7e65d64f04c4b8b6630cb229c6
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-03-27 16:54:58 +0000

    Temporarily delete all the "unhappy paths" until supported

--- cypress/e2e/not-eligible.cy.js
+++
@@ -1,9 +0,0 @@
-import './base.cy'
-
-describe('Registrant not eligible', () => {
-  it('Says registrant is not eligible if user selects other type of organisation', () => {
-    cy.goToRegistrantType()
-    cy.chooseRegistrantType(12)
-    cy.checkPageTitleIncludes('Your registrant is not eligible for a .gov.uk domain name')
-  })
-})
