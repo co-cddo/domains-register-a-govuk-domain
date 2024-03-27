de0f1858f8a0896d13ec0340d017bae3d6dd5529
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-03-27 15:28:25 +0000

    Route 3: local gov permission

--- cypress/e2e/happy-path-route-3.cy.js
+++ cypress/e2e/happy-path-route-3.cy.js
@@ -6,13 +6,10 @@ describe('Happy path - route 2 - 5', () => {
     cy.fillOutRegistrarDetails('WeRegister', 'Joe Bloggs', '01225672345', 'joe@example.org')
 
     cy.checkPageTitleIncludes('Who is this domain name for?')
-    cy.chooseRegistrantType(1) // Central government -> Route 2
-
-    cy.checkPageTitleIncludes('Why do you want a .gov.uk domain name?')
-    cy.chooseDomainPurpose(2) // Email address only -> Route 5
+    cy.chooseRegistrantType(5) // Fire service -> Route 3
 
     cy.checkPageTitleIncludes('Does your registrant have proof of permission to apply for a .gov.uk domain name?')
-    // TODO: will need to check it's the central gov version of the written-permission page
+    // TODO: will need to check it's the local gov version of the written-permission page
     cy.selectYesOrNo('written_permission', 'yes')
 
     cy.checkPageTitleIncludes('Upload evidence of permission to apply')
@@ -27,17 +24,9 @@ describe('Happy path - route 2 - 5', () => {
     cy.checkPageTitleIncludes('Can you confirm if the something-pc.gov.uk domain name is correct?')
     cy.selectYesOrNo('domain_confirmation', 'yes')
 
-    cy.checkPageTitleIncludes('Has a central government minister requested the something-pc.gov.uk domain name?')
-    cy.selectYesOrNo('minister', 'yes')
-
-    cy.checkPageTitleIncludes('Upload evidence of the minister\'s request')
-    cy.uploadDocument('image.png')
-
-    cy.checkPageTitleIncludes('Upload evidence of the minister\'s request')
-    cy.confirmUpload('image.png')
-
     cy.checkPageTitleIncludes('Registrant details')
-    cy.get('p').should('not.include.text', 'For example, for Parish Councils the registrant must be the Clerk.')
+    cy.get('p').should('include.text', 'For example, for Parish Councils the registrant must be the Clerk.')
+
     cy.fillOutRegistrantDetails('HMRC', 'Rob Roberts', '01225672344', 'rob@example.org')
 
     cy.checkPageTitleIncludes('Registrant details for publishing to the registry')
@@ -47,6 +36,5 @@ describe('Happy path - route 2 - 5', () => {
     cy.get('#button-continue').click()
 
     cy.checkPageTitleIncludes('Application submitted')
-
   })
 })
