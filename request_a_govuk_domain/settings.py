c6b10d6ab679291a61797cb810ef9e89e1155234
Jim Narey <james.narey@digital.cabinet-office.gov.uk>
2024-04-19 22:33:53 +0100

    Add Grappelli package

--- request_a_govuk_domain/settings.py
+++ request_a_govuk_domain/settings.py
@@ -40,6 +40,7 @@ ENVIRONMENT = env.str("ENVIRONMENT", default=None)
 # Application definition
 
 INSTALLED_APPS = [
+    "grappelli",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- request_a_govuk_domain/urls.py
+++ request_a_govuk_domain/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -113,6 +113,7 @@ urlpatterns = [
         WrittenPermissionUploadConfirmView.as_view(),
         name="written_permission_upload_confirm",
     ),
+    path("grappelli/", include("grappelli.urls")),
     path("admin/", admin.site.urls),
     path(
         "registrant-type-fail/",
