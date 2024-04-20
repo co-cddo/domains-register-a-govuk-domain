f5aea54761938672de4d7ddeb29537a7d4a915b6
Jim Narey <james.narey@digital.cabinet-office.gov.uk>
2024-04-20 16:21:57 +0100

    Allow null values in the Review model
    We want reviewers to be able to save reviews as they work on them,
    meaning that it's legitimate for fields to be blank on save. We
    enable null values at database and form level.
    
    Separate logic will be used when a reviewer attempts to approve
    or reject an application.

--- request_a_govuk_domain/request/models/review.py
+++ request_a_govuk_domain/request/models/review.py
@@ -18,30 +18,44 @@ class Review(models.Model):
     application = models.OneToOneField(Application, on_delete=models.CASCADE)
 
     registrant_org_exists = models.BooleanField(default=False)
-    registrant_org_exists_notes = models.TextField(max_length=NOTES_MAX_LENGTH)
+    registrant_org_exists_notes = models.TextField(
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
+    )
 
     registrant_org_eligible = models.BooleanField(default=False)
-    registrant_org_eligible_notes = models.TextField(max_length=NOTES_MAX_LENGTH)
+    registrant_org_eligible_notes = models.TextField(
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
+    )
 
     registrant_person_id_confirmed = models.BooleanField(default=False)
-    registrant_person_id_confirmed_notes = models.TextField(max_length=NOTES_MAX_LENGTH)
+    registrant_person_id_confirmed_notes = models.TextField(
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
+    )
 
     permission_signatory_role_confirmed = models.BooleanField(default=False)
     permission_signatory_role_confirmed_notes = models.TextField(
-        max_length=NOTES_MAX_LENGTH
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
     )
 
     domain_name_validated = models.BooleanField(default=False)
-    domain_name_validated_notes = models.TextField(max_length=NOTES_MAX_LENGTH)
+    domain_name_validated_notes = models.TextField(
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
+    )
 
     gds_exemption_validated = models.BooleanField(null=True)
-    gds_exemption_validated_notes = models.TextField(max_length=NOTES_MAX_LENGTH)
+    gds_exemption_validated_notes = models.TextField(
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
+    )
 
     ministerial_request_validated = models.BooleanField(null=True)
-    ministerial_request_validated_notes = models.TextField(max_length=NOTES_MAX_LENGTH)
+    ministerial_request_validated_notes = models.TextField(
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
+    )
 
     nac_appeal_validated = models.BooleanField(null=True)
-    nac_appeal_validated_notes = models.TextField(max_length=NOTES_MAX_LENGTH)
+    nac_appeal_validated_notes = models.TextField(
+        max_length=NOTES_MAX_LENGTH, blank=True, null=True
+    )
 
     history = HistoricalRecords()
 
