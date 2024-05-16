5fd5eda333a21caae3b875631d34caafd58f6105
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-05-16 13:12:29 +0100

    Remove possible PII leak to application logs

--- request_a_govuk_domain/request/views.py
+++ request_a_govuk_domain/request/views.py
@@ -615,10 +615,6 @@ class DomainPurposeFailView(FormView):
 
 
 def service_failure_view(request):
-    registration_data = request.session.get("registration_data", {})
-    logger.error(
-        f"500 Server Error occurred. Registration data: {registration_data}", exc_info=1
-    )
     request.session.pop("registration_data", None)
     return render(request, "500.html", status=500)
 
