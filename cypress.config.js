93b64f4dccb3c1d686577f6ac3d6e9b7117ac84b
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2024-05-20 17:02:36 +0100

    Run cypress tests in non-debug mode.
    It will make it possible to test error pages

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1500,
   e2e: {
-    baseUrl: 'http://0.0.0.0:8010/',
+    baseUrl: 'http://localhost:8010/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
