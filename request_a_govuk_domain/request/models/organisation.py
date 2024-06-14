fa302c2f88781ea1f576a1263bcaf8a05bb9de0e
Jim Narey <james.narey@digital.cabinet-office.gov.uk>
2024-06-14 15:07:20 +0100

    Change some local organisation names
    Change some of the names of local councils in the registrant choices
    in response to user feedback. Consequent changes to the journey logic.
    
    WIP - start fixing errors introduced into e2e tests.

--- request_a_govuk_domain/request/models/organisation.py
+++ request_a_govuk_domain/request/models/organisation.py
@@ -7,12 +7,9 @@ class RegistrantTypeChoices(models.TextChoices):
         "Central government department or agency"
     )
     ALB = "alb", _("Non-departmental body - also known as an arm's length body")
-    PARISH_COUNCIL = "parish_council", _("Parish, small town or community council")
-    LOCAL_AUTHORITY = "local_authority", _(
-        "Town, county, borough, metropolitan or district council"
-    )
+    PARISH_COUNCIL = "parish_council", _("Parish, town or community council")
+    LOCAL_AUTHORITY = "local_authority", _("District, borough, city or county council")
     FIRE_SERVICE = "fire_service", _("Fire service")
-    VILLAGE_COUNCIL = "village_council", _("Neighbourhood or village council")
     COMBINED_AUTHORITY = "combined_authority", _("Combined or unitary authority")
     PCC = "pcc", _("Police and crime commissioner")
     JOINT_AUTHORITY = "joint_authority", _("Joint authority")
