f308e4c33be8d178496815816646a64e167ca212
Sravan Jinna <524620+jinnashravan@users.noreply.github.com>
2025-01-28 20:36:56 +0000

    Cypress tests for session timeout.
    Using the clock and tick API's of cypress to override the time function.
    Warning appears after 15 minutes and session ends after 20 minutes if no action is taken.

---
+++ cypress/e2e/session_timeout.cy.js
@@ -0,0 +1,72 @@
+import './base.cy'
+
+describe('session timeout tests', () => {
+
+  beforeEach(()=>{
+    cy.clock();
+    cy.start()
+  });
+
+  it('session_timeout_dialog_success', () => {
+
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click();
+    cy.get('#id_registrar_organisation').clear().type('WeRegister')
+    cy.get('#id_registrar_name').clear('Test');
+    cy.get('#id_registrar_phone').clear('01225672345');
+    cy.get('#id_registrar_email').clear('test@example.gov.uk');
+    cy.get('#id_submit').click();
+
+    // clock tick
+    cy.tick(15 * 60 * 1000);
+    cy.get('#modal_dialog').should('exist');
+    cy.get('#modal_dialog').should('be.visible');
+
+  });
+
+  it('session_timeout_click_continue_session', () => {
+
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click();
+    cy.get('#id_registrar_organisation').clear().type('WeRegister')
+    cy.get('#id_registrar_name').clear('Test');
+    cy.get('#id_registrar_phone').clear('01225672345');
+    cy.get('#id_registrar_email').clear('test@example.gov.uk');
+    cy.get('#id_submit').click();
+
+    // clock tick
+    cy.tick(15 * 60 * 1000); // 1 minute for testing
+    cy.get('#modal_dialog').should('exist');
+    cy.get('#modal_dialog').should('be.visible');
+
+    // Click the "Continue" button
+    cy.get('#session-continue').click();
+
+    // Check if the warning modal is hidden
+    cy.get('#modal_dialog').should('not.be.visible');
+
+  });
+
+  it('session_timeout_session_ended', () => {
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').click();
+    cy.get('#id_registrar_organisation').clear().type('WeRegister')
+    cy.get('#id_registrar_name').clear().type('Test');
+    cy.get('#id_registrar_phone').clear().type('01225672345');
+    cy.get('#id_registrar_email').clear().type('test@example.gov.uk');
+    cy.get('#id_submit').click();
+
+    // Simulate 15 minutes of inactivity
+    cy.tick(15 * 60 * 1000); // 15 minutes
+
+    // Ensure the modal dialog exists and is visible
+    cy.get('#modal_dialog').should('exist');
+    cy.get('#modal_dialog').should('be.visible');
+
+    // Simulate additional 1 minutes of inactivity
+    cy.tick(5 * 60 * 1000); // 1 minutes
+
+    // Check if the warning modal is still hidden
+    cy.get('#modal_dialog').should('not.be.visible');
+
+    // Check if the user is not redirected to the session-ended page
+    cy.url().should('include', '/session-ended/');
+  });
+})
