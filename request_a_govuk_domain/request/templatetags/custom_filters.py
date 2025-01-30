225cb3d1577dbfabc7e29ab3fb650e09520bd6a7
Sravan Jinna <524620+jinnashravan@users.noreply.github.com>
2025-01-30 21:43:17 +0000

    Fixing Indentation and added type hints for template tags.

--- request_a_govuk_domain/request/templatetags/custom_filters.py
+++ request_a_govuk_domain/request/templatetags/custom_filters.py
@@ -1,10 +1,11 @@
+from typing import Any
 from django import template
 
 register = template.Library()
 
 
 @register.filter
-def divide_by_60(value):
+def divide_by_60(value: Any) -> Any:
     try:
         return int(value) // 60
     except (ValueError, TypeError):
