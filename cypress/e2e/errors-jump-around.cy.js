f891550831c55f608faab6d14c7d4a15e6cd26b8
Max Froumentin <github@e97.me>
2025-03-04 16:42:11 +0000

    Correctly indent and format

--- cypress/e2e/errors-jump-around.cy.js
+++ cypress/e2e/errors-jump-around.cy.js
@@ -1,48 +1,44 @@
 import './base.cy'
 
 describe('Errors when user skips and jumps pages', () => {
-    it('Throws a 400 when user skips to success page', () => {
-
-        cy.goToRegistrantDetails()
-
-        cy.request({url: '/success', failOnStatusCode: false}).then(res => {
-            expect(res.status).to.eq(400)
-        })
+  it('Throws a 400 when user skips to success page', () => {
+    cy.goToRegistrantDetails()
+    cy.request({url: '/success', failOnStatusCode: false}).then(res => {
+      expect(res.status).to.eq(400)
     })
+  })
 
-    it('Throws a 400 when user goes back 6 pages after submitting', () => {
-        cy.goToConfirmation(1)
-        cy.get('.govuk-button#id_submit').click()
-        cy.checkPageTitleIncludes('Application submitted')
-        cy.go(-6)
-        cy.checkPageTitleIncludes('Invalid request')
-    })
+  it('Throws a 400 when user goes back 6 pages after submitting', () => {
+    cy.goToConfirmation(1)
+    cy.get('.govuk-button#id_submit').click()
+    cy.checkPageTitleIncludes('Application submitted')
+    cy.go(-6)
+    cy.checkPageTitleIncludes('Invalid request')
+  })
 
-    it('Throws a 400 when user goes back 4 pages after submitting', () => {
-        cy.goToConfirmation(1)
-        cy.get('.govuk-button#id_submit').click()
-        cy.checkPageTitleIncludes('Application submitted')
-        cy.go(-4)
-        cy.checkPageTitleIncludes('Invalid request')
-    })
+  it('Throws a 400 when user goes back 4 pages after submitting', () => {
+    cy.goToConfirmation(1)
+    cy.get('.govuk-button#id_submit').click()
+    cy.checkPageTitleIncludes('Application submitted')
+    cy.go(-4)
+    cy.checkPageTitleIncludes('Invalid request')
+  })
 
-    it('Throws a 400 when user goes back and forth after submitting', () => {
-        cy.goToConfirmation(1)
-        cy.get('.govuk-button#id_submit').click()
-        cy.checkPageTitleIncludes('Application submitted')
-        cy.go(-4)
-        cy.checkPageTitleIncludes('Invalid request')
-        cy.go(3)
-        cy.checkPageTitleIncludes('Invalid request')
-    })
+  it('Throws a 400 when user goes back and forth after submitting', () => {
+    cy.goToConfirmation(1)
+    cy.get('.govuk-button#id_submit').click()
+    cy.checkPageTitleIncludes('Application submitted')
+    cy.go(-4)
+    cy.checkPageTitleIncludes('Invalid request')
+    cy.go(3)
+    cy.checkPageTitleIncludes('Invalid request')
+  })
 
-    it('Throws a 400 when user goes back after submitting', () => {
-        cy.goToConfirmation(1)
-        cy.get('.govuk-button#id_submit').click()
-        cy.checkPageTitleIncludes('Application submitted')
-        cy.go('back')
-        cy.checkPageTitleIncludes('Invalid request')
-    })
-
-
+  it('Throws a 400 when user goes back after submitting', () => {
+    cy.goToConfirmation(1)
+    cy.get('.govuk-button#id_submit').click()
+    cy.checkPageTitleIncludes('Application submitted')
+    cy.go('back')
+    cy.checkPageTitleIncludes('Invalid request')
+  })
 })
