87bd4042865af7bdfadc741a8e3288487ba27bef
Dharshana Vanderbona <dharshana.vanderbona@digital.cabinet-office.gov.uk>
2025-04-11 11:15:19 +0100

    enable storage

--- .github/workflows/start-server.sh
+++ .github/workflows/start-server.sh
@@ -2,4 +2,6 @@
 DEBUG=False
 export TEST_CSP=yes
 echo "Starting test server with DEBUG set to ${DEBUG}"
+curl --request GET -sL \
+     --url 'http://localhost:9090/favicon.ico'
 GOOGLE_ANALYTICS_ID=GTM-TEST poetry run ./manage.py runserver 0.0.0.0:8010
