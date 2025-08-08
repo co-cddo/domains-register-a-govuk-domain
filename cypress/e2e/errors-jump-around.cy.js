a22c6caac90b0468981a94158d0dac7a46cf7d7e
Max Froumentin <github@e97.me>
2025-08-08 15:04:45 +0100

    Make sure a new session is reset when necessary
    1. When a new application starts
       (Not strictly necessary, but for doesn't hurt)
    
    2. When the user reaches the success page

--- cypress/e2e/errors-jump-around.cy.js
+++ cypress/e2e/errors-jump-around.cy.js
@@ -2,6 +2,42 @@ import './base.cy'
 
 describe('Errors when user skips and jumps pages', () => {
 
+  it('Restarts a new session after completing an application', () => {
+    let sessionCookie1;
+
+    cy.deleteAllApplications()
+    cy.visit('/')
+
+    // First application
+    cy.visit('/registrar-details/')
+    cy.fillOutRegistrarDetails('WeRegister', 'Joe Bloggs', '01225672345', 'simulate-delivered@notifications.service.gov.uk')
+
+    cy.getCookie('sessionid').then(cookie => {
+      sessionCookie1 = cookie.value;
+    })
+
+    cy.checkPageTitleIncludes('Who is this domain name for?')
+    cy.chooseRegistrantType(3) // Parish, small town or community council -> route 1
+    cy.checkPageTitleIncludes('Choose a .gov.uk domain name')
+    cy.enterDomainName('something-pc')
+    cy.selectYesOrNo('domain_confirmation', 'yes')
+    cy.get('p').should('include.text', 'For example, for parish councils the registrant must be the Clerk.')
+    cy.fillOutRegistrantDetails('HMRC', 'Rob Roberts', '01225672344', 'rob@example.org')
+    cy.fillOutRegistryDetails('Clerk', 'clerk@example.org')
+    cy.summaryShouldHave(3, 'something-pc.gov.uk')
+    cy.get('#id_submit').click()
+
+    // Second application
+    cy.visit('/registrar-details/')
+    cy.fillOutRegistrarDetails('WeRegister', 'Joe Bloggs', '01225672345', 'simulate-delivered@notifications.service.gov.uk')
+
+    // We should be starting a new session here
+    cy.getCookie('sessionid').should(cookie => {
+      expect(cookie).to.not.be.null;
+      expect(cookie.value).to.not.equal(sessionCookie1)
+    })
+  })
+
   it('Throws a 400 when user skips to registry page', () => {
     cy.goToRegistrantType()
     cy.request({ url: '/registry-details', failOnStatusCode: false })
